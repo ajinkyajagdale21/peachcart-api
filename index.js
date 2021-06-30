546a26260c15ec29159a0781aae736eb2748863c
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2021-06-30 14:08:18 +0530

    update: initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 8000
+
+const cors = require('cors')
+
+app.get('/', (req, res) => {
+    res.json({ test: "succesfull" })
+})
+
+app.listen(PORT, () => console.log('server started at port', PORT))
